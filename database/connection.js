caa049a1e33f09b64c70f6e9e004ca87381445d8
dhirajOpeninfo <dhirajs.openinfotech@gmail.com>
2023-10-06 21:22:18 +0530

    add env variable

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,8 @@
 const mongoose =  require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
-
-mongoose.connect("mongodb://localhost:27017/crud-template").then(()=>{
+mongoose.connect(process.env.DATABASE_URL).then(()=>{
    console.log("Connected to database")
 }).catch(()=>{
    console.log("Connection Unsuccessfull")
